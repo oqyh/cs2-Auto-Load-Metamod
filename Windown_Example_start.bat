dbe61944def10ca7e2f959e5176e190fb8d35e2d
Gold KingZ <48490385+oqyh@users.noreply.github.com>
2024-07-08 18:39:34 +0400

    Update Windown_Example_start.bat

--- Windown_Example_start.bat
+++ Windown_Example_start.bat
@@ -2,5 +2,4 @@ powershell -ExecutionPolicy Bypass -File .\modify_gameinfo.ps1
 
 cd /D D:\Local_Host\CS2\game\bin\win64
 start /min cs2.exe -dedicated -usercon -console -insecure -dev -maxplayers 10 -port 27016 +game_type 0 +game_mode 0 +mapgroup mg_comp +sv_lan 1 +map de_dust2 +sv_setsteamaccount "XXXXXXXXXXXXXXXXXX"
-pause
 exit
